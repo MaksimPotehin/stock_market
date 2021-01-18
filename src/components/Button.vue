99c1a71df720be04150ebd5fe6c03af91ca757d6
maks <MaksP21@gmail.com>
2021-01-18 16:11:04 +0200

    created reusable input and button

---
+++ src/components/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button :class="[inputStyle, { 'opacity-60 pointer-events-none':disabled }]" class="button" @click.prevent="click">
+    <span>{{ text }}</span>
+    <div>{{ iconName }}</div>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  computed: {
+    inputStyle() {
+      return '' +
+          'w-28 border border-blue-400' +
+          ' outline-none focus:outline-none transition-all' +
+          ' duration-500 rounded-md' +
+          ' hover:bg-blue-400 cursor-pointer py-2'
+    }
+  },
+  props: {
+    text: {
+      type: String,
+      default: '',
+      required: true
+    },
+    iconName: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    click() {
+      this.$emit('handleClick')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
