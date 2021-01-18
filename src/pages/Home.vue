6bf20e3ee49ac71948daaedad7348c7811b1216c
maks <MaksP21@gmail.com>
2021-01-18 17:39:29 +0200

    refactored Home page with tailwind

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <el-main class="home">
-    <div class="card">
+  <main class="p-5 max-w-screen-2xl w-full">
+    <div class="p-5 border border-gray-400 mx-auto my-12 text-left">
       <h2>Trade or View your portfolio</h2>
       <p>You may "save & load" your data</p>
       <p>Click on "End Day" to begin a new Day</p>
       <p>I sit at my window this morning where the world like a passer-by stops for a moment, nods to me and goes.</p>
-      <el-divider></el-divider>
+      <hr class="my-8">
       <h3>Your Funds: {{ funds | changeFunds }}</h3>
     </div>
-  </el-main>
+  </main>
 </template>
 
 <script>
@@ -22,13 +22,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.card {
-  /*max-width: 1200px;*/
-  margin: 50px auto;
-  padding: 20px;
-  border: 1px solid #929292;
-  text-align: left;
-}
-</style>
