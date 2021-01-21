054fdc446c5ebaddd09a678a847cf3686ee3f978
maks <MaksP21@gmail.com>
2021-01-21 12:34:23 +0200

    has been deleted loader module and moved loader to store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { START_LOADING, STOP_LOADING } from '@/store/type'
+
 import portfolio from '@/store/module/portfolio'
 import stocks from '@/store/module/stocks'
-import loader from '@/store/module/loader'
 import loadData from '@/store/module/loadData'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state: {
+    isLoading: false
+  },
+  mutations: {
+    [START_LOADING](state) {
+      state.isLoading = true
+    },
+    [STOP_LOADING](state) {
+      state.isLoading = false
+    }
+  },
+  getters: {
+    isLoading(state) {
+      return state.isLoading
+    }
+  },
   modules: {
     portfolio,
     stocks,
-    loader,
     loadData
   }
 })
